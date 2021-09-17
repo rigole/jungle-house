3d4b24435a5a232ba7aa948287d62b918fd2f31f
rigole <foplacide@gmail.com>
2021-09-17 21:40:58 +0100

    styling the cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,22 +4,23 @@ import { useState } from "react";
 function Cart() {
     const monsteraPrice = 8
     const [cart, updateCart] = useState(0)
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
 
     return isOpen ? (
-        <div>
-            <button onClick={() => setIsOpen(false)}>Fermer</button>
+        <div className='lmj-cart'>
+            <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>Fermer</button>
             <h2 className='lmj-cart'>Panier</h2>
             <div>
                 Monstera : {monsteraPrice}$
-                <button onClick={() => updateCart(cart + 1)}>
-                    Ajouter
-                </button>
+                <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
             </div>
             <h3>Total : {monsteraPrice * cart}$</h3>
         </div>
     ) : (
-        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+        <div className='lmj-cart-closed'>
+            <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+        </div>
+
     )
 }
 
